77e86a2d6409a2b4dd0775c21356ca613e067b1e
Reshet <reshet@itsyndicate.org>
2014-12-08 17:23:03 +0200

    Изменена логика работы скрипта

--- useradd.sh
+++ useradd.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-#Empty check function
-function empty_check {
-	if [ -z $1 ]; then
-		echo "Error: Variable could not be empty"
-	exit 1
-	fi
-}
-
-#Input data
 read -p "Type name of the new user: " USERNAME
-empty_check $USERNAME
+if [ -z $USERNAME ]; then
+	echo "Error: username could not be empty!"
+	exit 1
+fi
+
 read -sp "Type password of the new user (will be not displayed): " USERPASS
-empty_check $USERPASS
 echo
+
 read -p "If you have SSH-key, type it: " SSHKEY
+if [[ -z $USERPASS && -z $SSHKEY ]]; then
+        echo "Error: you should provide a password and/or SSH-key for user!"
+        exit 1
+fi
+
+read -p "Would you like grant sudo access for user? (y/N)" GRANTSUDO
 
 for serv in $(cat ipfile.txt); do
 	echo -n "Adding user ${USERNAME} to host ${serv}... "
@@ -24,13 +25,17 @@ for serv in $(cat ipfile.txt); do
 		exit 1
 	fi
 	ssh -q $serv /usr/sbin/useradd -m -s /bin/bash $USERNAME
-	ssh -q $serv 'echo "'${USERNAME}':'${USERPASS}'" | /usr/sbin/chpasswd'
+	if [ -n "$USERPASS" ]; then
+		ssh -q $serv 'echo "'${USERNAME}':'${USERPASS}'" | /usr/sbin/chpasswd'
+	fi
 	if [ -n "$SSHKEY" ]; then
 		ssh -q $serv mkdir /home/${USERNAME}/.ssh
 		ssh -q $serv 'echo "'$SSHKEY'" > /home/'${USERNAME}'/.ssh/authorized_keys'
 		ssh -q $serv chown -R ${USERNAME}.${USERNAME} /home/${USERNAME}
 	fi
-	ssh -q $serv 'echo "'${USERNAME}' ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers'
-	ssh -q $serv 'echo "Defaults:'${USERNAME}' !requiretty" >> /etc/sudoers'
+	if [[ $GRANTSUDO = "y" || $GRANTSUDO = "Y" ]]; then
+		ssh -q $serv 'echo "'${USERNAME}' ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers'
+		ssh -q $serv 'echo "Defaults:'${USERNAME}' !requiretty" >> /etc/sudoers'
+	fi
 	echo 'Done'
 done
